aee371617cfa981837ebb60ad0515264b8ed7f1e
Jobst Heitzig <konsument@posteo.de>
2019-03-12 23:19:39 +0100

    anim started

--- src/app/closedpoll/closedpoll.page.ts
+++ src/app/closedpoll/closedpoll.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from "../global.service";
 
 @Component({
   selector: 'app-closedpoll',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClosedpollPage implements OnInit {
 
-  constructor() { }
+  constructor(public g: GlobalService) { }
 
   ngOnInit() {
   }
