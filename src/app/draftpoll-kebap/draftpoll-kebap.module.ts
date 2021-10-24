331a0e34a8e73ab28ebb6f5f51faa0554807ead9
Jobst Heitzig <heitzig@pik-potsdam.de>
2021-10-24 23:04:22 +0200

    i18n settings, draftpoll done

--- src/app/draftpoll-kebap/draftpoll-kebap.module.ts
+++ src/app/draftpoll-kebap/draftpoll-kebap.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -14,7 +15,8 @@ export { DraftpollKebapPage } from './draftpoll-kebap.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    DraftpollKebapPageRoutingModule
+    DraftpollKebapPageRoutingModule,
+    TranslateModule.forChild()
   ],
   declarations: [DraftpollKebapPage],
   exports: [DraftpollKebapPage]
