fb440d36ee20720b9d70e55a142170265fcdf5ac
heitzig <heitzig@pik-potsdam.de>
2022-04-21 19:05:31 +0200

    also for user docs belonging to polls

--- couchdb/vodle/_design/vodle/views/poll_due_doc_by_doc_id/map.js
+++ couchdb/vodle/_design/vodle/views/poll_due_doc_by_doc_id/map.js
@@ -2,7 +2,7 @@
  * helper view for lists/poll_docs_by_due
  */
 function(doc) {
-    const pollprefix = "~vodle.poll.";
+    const userprefix = "~vodle.user.", pollprefix = "~vodle.poll.";
     let _id = doc._id;
     if (_id.startsWith(pollprefix)) {
         var end = _id.slice(pollprefix.length),
@@ -12,5 +12,15 @@ function(doc) {
             pid = end.slice(0, pos),
             due_doc_id = pollprefix + pid + "§due";
         emit(doc._rev, {_id: due_doc_id});
+    } else if (_id.startsWith(userprefix)) {
+        var pos_par = _id.indexOf('§'),
+            key = _id.slice(pos_par+1);
+        if (key.startsWith('poll.')) {
+            var end = key.slice('poll.'.length),
+                pos = end.indexOf('.')
+                pid = end.slice(0, pos),
+                due_doc_id = pollprefix + pid + "§due";
+            emit(doc._rev, {_id: due_doc_id});
+        }
     }
 }
