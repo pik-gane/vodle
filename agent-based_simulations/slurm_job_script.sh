3961116c81ce2ac15f2aac15fa1a43dba4c06fb1
heitzig <heitzig@pik-potsdam.de>
2023-08-07 11:40:10 +0200

    small fix

---
+++ agent-based_simulations/slurm_job_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --qos=short
+#SBATCH --job-name=maxparc
+#SBATCH --account=heitzig
+#SBATCH --output=test-%j.out
+#SBATCH --error=test-%j.err
+#SBATCH --workdir=/p/tmp/heitzig/
+
+module load anaconda/2020.11
+conda activate /p/projects/ou/labs/gane/maxparc/paper_revision/conda_env
+python -u /p/projects/ou/labs/gane/maxparc/paper_revision/vodle/agent-based_simulations/src/maxparc_paper_simulations.py
