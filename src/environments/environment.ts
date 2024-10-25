bfa4ea36953ae0757d14d7e298ba6124ebd9bdb9
HEMPED~ <Hemanath.Peddireddi@warwick.ac.uk>
2024-10-25 13:24:53 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -52,7 +52,7 @@ export const environment = {
 //    backdoor_public_key: "ea17226c631a8a78c67626136d91980e82328b72e6b536c7df7e68fbb22c2aa7",
   },
   delegation: {
-    enabled: true,
+    enabled: false,
     max_weight: 3
   },
   no_more_options_time_fraction: 1/2,
